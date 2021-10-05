fdada7b04cd7fd5d19758b6a6c325e3d8c10532f
mkaufmann84 <max906993@gmail.com>
2021-10-04 20:18:08 -0400

    fixed some not all memory leak issues.

--- static/main.js
+++ static/main.js
@@ -19,6 +19,7 @@ var model = load();
 var model_s = load_s();
 var model_class =load_class();
 
+
 var debugc = document.querySelector("#debugc")
 console.log(debugc,"here")
 debugc.innerHTML = "Debug"
@@ -28,7 +29,7 @@ console.log("laoded")
 document.querySelector('#file_upload').addEventListener('change', function() {
   console.log("change")
   
-  const fileInput = document.getElementById('file_upload').files[0];
+  let fileInput = document.getElementById('file_upload').files[0];
   console.log(fileInput,"finput");
   debugc.innerHTML = "found file"
   if (fileInput.name.includes("HEIC"))
@@ -42,8 +43,8 @@ document.querySelector('#file_upload').addEventListener('change', function() {
       .then((blob) => heic2any({ blob }))
       .then((conversionResult) => {
           console.log("conversion result")
-          blobby = conversionResult// conversionResult is a BLOB
-          url = URL.createObjectURL(blobby)
+          let blobby = conversionResult// conversionResult is a BLOB
+          let url = URL.createObjectURL(blobby)
           debugc.innerHTML = "blobby created. solving"
           solve(url)
 
@@ -59,7 +60,7 @@ document.querySelector('#file_upload').addEventListener('change', function() {
   {
     debugc.innerHTML = "non-heic is running"
     console.log("Non-HEIC")
-    readerDim = new FileReader();
+    let readerDim = new FileReader();
     readerDim.readAsDataURL(fileInput);
     console.log(readerDim,"readerDim")
     debugc.innerHTML ="waiting for non heic .onload"
@@ -77,82 +78,102 @@ document.querySelector('#file_upload').addEventListener('change', function() {
 
 
   function solve(url_to_src) {
+
     debugc.innerHTML = "made it to solve"
-    var image = new Image();
+    let image = new Image();
     //Set the Base64 string return from FileReader as source.
     image.src = url_to_src
 
     //Validate the File Height and Width.
     image.onload = function () {
       debugc.innerHTML = "image loaded"
-      //var height = this.height;
-      //var width = this.width;
 
-      var a = tf.browser.fromPixels(image);
-      console.log(a.shape,image.width,image.height)
-      
+
       //convert image into 3 channel grayscale
-      var g_scale =a.mean(2).expandDims(-1);
-      var a = tf.image.grayscaleToRGB(g_scale).cast("int32");
 
-      a=a.expandDims();
-      //tf.browser.toPixels(a.squeeze(),canvas);
-      debugc.innerHTML = "a loaded"
+
+      let a= tf.tidy(() => {
+        let og = tf.browser.fromPixels(image);
+        let g_scale =og.mean(2).expandDims(-1);
+        let al = tf.image.grayscaleToRGB(g_scale).cast("int32");
+        let ald = al.expandDims();
+        console.log(ald.shape)
+        if (ald.shape[1]>1000||ald.shape[2]>1000)
+        {
+
+            let max = Math.max(ald.shape[1],ald.shape[2]);
+
+            //x*max = 1000
+            let percent = 1000/max;
+            let news = [Math.round(ald.shape[1]*percent),Math.round(ald.shape[2]*percent)];
+            ald = tf.image.resizeBilinear(ald.cast('float32').div(255.0),news);
+            ald = ald.mul(255).cast("int32");
+
+            console.log(ald.shape)
+        }
+        return ald
+      });
       
+      debugc.innerHTML = "a loaded"
+      console.log("1",tf.memory())
+
       model.then(model => {
       
       async function pred() {
         console.log('Start');
-        const result = await model.executeAsync(a);
+        
         debugc.innerHTML = "made first predictions"
-        //const classes = await result[2].array();
-        //const accuracy = await result[4].array();
-        const boxes = await result[6].array(); //y,x,height,width
+        let result = await model.executeAsync(a);
+        let boxes = await result[6].array(); //y,x,height,width
+        let fin = boxes[0][0];
+
+
+        
         
         //Time to draw 
-        const canvas = document.querySelector('#canvas');
-        const ctx = canvas.getContext('2d');
+        let canvas = document.querySelector('#canvas');
+        let ctx1 = canvas.getContext('2d');
+        ctx1.clearRect(0, 0, canvas.width, canvas.height)
         //resize
 
         canvas.height = image.height;
         canvas.width= image.width;
   
        // ctx.drawImage(image,0,0);
-       //tf.browser.toPixels(a.squeeze(),canvas);
-        const [y,x,height,width] =boxes[0][0];//x1, y1, x2, y2, not height or width
+       tf.tidy(()=>{
+           tf.browser.toPixels(a.squeeze(),canvas)});
 
-        const xc = Math.round(x*a.shape[2]);
-        const yc = Math.round(y*a.shape[1]);
-        const x2c = Math.round(width*a.shape[2]);
-        const y2c = Math.round(height*a.shape[1]);
+        let [y,x,height,width] = fin
 
-        var sliced = a.slice([0,yc,xc,0],[1,y2c-yc,x2c-xc,3] );
 
-        //tf.browser.toPixels(sliced.squeeze(),canvas2);
+
+        let xc = Math.round(x*a.shape[2]);
+        let yc = Math.round(y*a.shape[1]);
+        let x2c = Math.round(width*a.shape[2]);
+        let y2c = Math.round(height*a.shape[1]);
+
+        let sliced = a.slice([0,yc,xc,0],[1,y2c-yc,x2c-xc,3] );
+        a.dispose()
+
+        
 
 
         model_s.then(model_s =>{
           async function small_g(sliced){
             console.log("predicting small",sliced);
-            const result_s = await model_s.executeAsync(sliced);
-           // console.log(result_s)
-            const boxes1 = await result_s[6].array();
-            //0 second number in arrays is the same as output 4
-            //1 is weird. Large numbers
-            //2 is classes
-            //3 is a big array with size 4 negative numbs tho
-            //4 is a single with maybe accuracy but it is odd cause numbers drop off quick
-            //5 is a large array with 0.001 ranges of num
-            //6 is apparently the classes
-            //7 is num of predictions?
+            let result_s = await model_s.executeAsync(sliced);
+            let boxes1 = await result_s[6].array();
+            let b81 = boxes1[0].splice(0,81);
+ 
 
-            //const [y1,x1,height1,width1] =boxes1[0][0];
+            let indmap = new Map()
 
-            b81 = boxes1[0].splice(0,81)
-            var indmap = new Map()
-
-            const canvas_2 = document.querySelector('#canvas2');
-            const ctx = canvas_2.getContext('2d');
+            let canvas_2 = document.querySelector('#canvas2');
+            let ctx2 = canvas_2.getContext('2d');
+            ctx2.clearRect(0, 0, canvas_2.width, canvas_2.height)
+            
+            await tf.browser.toPixels(sliced.squeeze(),canvas2);
+            
 
             for (let i=0; i<81;i++){
 
@@ -163,11 +184,11 @@ document.querySelector('#file_upload').addEventListener('change', function() {
               let x21 = Math.round(b81[i][3]*sliced.shape[2])
 
 
-              //ctx.rect(x11,y11 , x21-x11, y21-y11);
-              //ctx.stroke();
+              ctx2.rect(x11,y11 , x21-x11, y21-y11);
+              ctx2.stroke();
             }
-            //console.log(indmap.get(0))
-            var y1Ordered = [0]
+
+            let y1Ordered = [0]
 
             Array.prototype.insert = function ( index, item ) {
               this.splice( index, 0, item );
@@ -198,7 +219,7 @@ document.querySelector('#file_upload').addEventListener('change', function() {
                 }
               }
             }
-            var strata = [];
+            let strata = [];
             for (let i =0;i<9;i++){
               let unorder= y1Ordered.splice(0,9);//splice doesnt need to change bounds because it mutates the array
               let order = [ unorder[0] ];
@@ -240,7 +261,8 @@ document.querySelector('#file_upload').addEventListener('change', function() {
             console.log("drawing...");
 
             let iter1=tf.fill([81], 0).cast('int32')
-            var cropped = tf.image.cropAndResize(sliced,b81,iter1,[32,32],'bilinear');
+            let cropped = tf.image.cropAndResize(sliced,b81,iter1,[32,32],'bilinear');
+            sliced.dispose();
             console.log(cropped,"cropped")
 
             
@@ -263,53 +285,55 @@ document.querySelector('#file_upload').addEventListener('change', function() {
                   } //end function bracket
               console.log("---1---")
 
-              var l_inputs = document.querySelectorAll('input');
+              let l_inputs = document.querySelectorAll('input');
               for (let group =0;group<9;group++)
               {
                 for (let i =0;i<9;i++)
                 {
-                  var test_cropped = cropped.slice([strata[group][i],0,0,0],[1,32,32,3]);
+                  let test_cropped = tf.tidy(()=>{
+                      let tmp = cropped.slice([strata[group][i],0,0,0],[1,32,32,3])
+                      let test_cropped = tmp.mean(3);
+                      test_cropped = test_cropped.expandDims(-1)
+                      return test_cropped
+                    });
                   //ctx.clearRect(0, 0, canvas.width, canvas.height)
                   //tf.browser.toPixels(test_cropped.squeeze().cast('int32'),canvas);
-                  test_cropped = test_cropped.mean(3);
-                  test_cropped = test_cropped.expandDims(-1)
+
                   
-                  var result_class = await model_class.execute(test_cropped).array();
-                  var ind = index_max(result_class[0])
+                  let result_class = await model_class.execute(test_cropped).array();
+                  let ind = index_max(result_class[0])
+
                   if(ind==0){
                     ind=''
                   }
                   l_inputs[(group*9)+i].value = ind
+                  test_cropped.dispose()
 
                   //await new Promise(r => setTimeout(r, 1500));
                 }
               }
-              
-              console.log("-----2------")
+            
 
               }
               ttt()
+              //cropped.dispose()
 
             })
          
          
           }
-          //console.log(sliced.dataSync())
 
-          const result_s = small_g(sliced);
+
+          small_g(sliced);
 
         });
         };
-      console.log('Predicting')
-      var box =  pred();
 
-      }).catch((e) =>{
-        debugc.inerHTML ="fail at model load"
-        debugc.inerHTML = e
-        
-    });
-    }
+      pred();
 
+      });
+
+    };
   };
 
 
